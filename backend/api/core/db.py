fa1cfa5414d756153960be3822a614579aa4607f
Kerem Vatandas <keremvatandas@gmail.com>
2021-12-12 00:52:54 +0300

    impl. database connection and session

---
+++ backend/api/core/db.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from core.config import DEBUG, ENCODING, SQLALCHEMY_DATABASE_URI
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URI,
+    encoding=ENCODING,
+    echo=DEBUG,
+    connect_args={"check_same_thread": False}
+)
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+def get_db():
+    try:
+        db = Session()
+        yield db
+    finally:
+        db.close()
