86acdf64a421bc4a26bac231b5fb11436cd8913c
Kerem Vatandas <keremvatandas@gmail.com>
2021-12-12 01:15:54 +0300

    added insert dummy data to sqlite and upd. gitignore

--- backend/api/main.py
+++ backend/api/main.py
@@ -3,12 +3,14 @@ from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 
 from core.config import API_VERSION, DEBUG, PROJECT_NAME, SECRET_KEY
-from core.db import Base, engine
+from core.db import Base, Session, engine
 from models import app, screenshot
+from resources.init_data import read_and_prepare_data
 
 
 def start_app() -> FastAPI:
     Base.metadata.create_all(engine)
+    create_dummy_data()
     app = FastAPI(title=PROJECT_NAME, debug=DEBUG, version=API_VERSION)
     app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
     app.add_middleware(
@@ -19,6 +21,20 @@ def start_app() -> FastAPI:
         allow_headers=["*"],
     )
     return app
+
+
+def create_dummy_data():
+    import os
+    from pathlib import Path
+
+    path = f"{os.getcwd()}/.created"
+    if not os.path.exists(path):
+        apps, ss = read_and_prepare_data()
+        with Session() as session:
+            session.bulk_save_objects(apps)
+            session.bulk_save_objects(ss)
+            session.commit()
+        Path(path).touch()
 
 
 app = start_app()
---
+++ backend/api/resources/init_data.py
@@ -0,0 +1,36 @@
+import csv
+import os
+from datetime import datetime
+
+from models import app, screenshot
+
+
+def read_and_prepare_data():
+    sample_apps_csv = f"{os.getcwd()}/resources/sample_apps.csv"
+    sample_ss_csv = f"{os.getcwd()}/resources/sample_screeshots.csv"
+
+    app_reader = csv.DictReader(open(sample_apps_csv))
+    ss_reader = csv.DictReader(open(sample_ss_csv))
+
+    appsObj = []
+    for item in app_reader:
+        appsObj.append(
+            app.App(
+                id=item["id"],
+                name=item["name"],
+                icon=item["icon"],
+                created_at=datetime.strptime(item["created_at"], "%Y-%m-%d %H:%M:%S"),
+            )
+        )
+
+    ssObj = []
+    for item in ss_reader:
+        ssObj.append(
+            screenshot.Screenshot(
+                id=item["id"],
+                app_id=item["app_id"],
+                file_name=item["file_name"],
+                created_at=datetime.strptime(item["created_at"], "%Y-%m-%d %H:%M:%S"),
+            )
+        )
+    return (appsObj, ssObj)
